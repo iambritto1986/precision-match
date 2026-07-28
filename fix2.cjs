7d3b10f41f5fe5bcf721a59248ec6c56aa31c72a
Your Name <you@example.com>
2026-07-28 16:33:27 -0500

    chore: remove unused scratch scripts and duplicate CSS, add og-image

--- fix2.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/components/ResumeTemplates.tsx', 'utf8');
-code = code.replace(/pageBreaks\?\.\[\]/g, 'pageBreaks?.[elementId]');
-fs.writeFileSync('src/components/ResumeTemplates.tsx', code);
-console.log('Fixed');
--- fix_email_dashboard.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let app = fs.readFileSync('src/components/FounderDashboard.tsx', 'utf8');
-app = app.replace(/iambritto1986@gmail\.com/g, 'iambrittothomas@gmail.com');
-fs.writeFileSync('src/components/FounderDashboard.tsx', app);
-console.log('Email corrected in FounderDashboard.tsx');
